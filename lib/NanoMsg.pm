543fdea201a59b0e4eeff7402fe33b3cd791d303
Florian Ragwitz <rafl@debian.org>
2013-08-16 01:54:49 -0400

    Export all symbols

--- lib/NanoMsg.pm
+++ lib/NanoMsg.pm
@@ -4,6 +4,16 @@ use strict;
 use warnings;
 use XSLoader;
 
-XSLoader::load __PACKAGE__; # TODO: version
+BEGIN {
+    XSLoader::load __PACKAGE__; # TODO: version
+}
+
+use Exporter 'import';
+
+our @EXPORT = (
+    (map { "nn_$_" } qw(socket close setsockopt getsockopt bind connect shutdown
+                       send recv device term)),
+    qw(AF_SP NN_PAIR),
+);
 
 1;
