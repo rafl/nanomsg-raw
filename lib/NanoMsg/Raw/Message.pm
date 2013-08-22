6c6ddca30e2925a7383ffe84196a75a6cc199fac
Florian Ragwitz <rafl@debian.org>
2013-08-22 14:58:41 -0400

    More ::Message docs

--- lib/NanoMsg/Raw/Message.pm
+++ lib/NanoMsg/Raw/Message.pm
@@ -21,6 +21,33 @@ use overload '""' => sub { ${ $_[0] } }, fallback => 1;
         warn $buf;
     }
 
+=head1 DESCRIPTION
+
+Instances of this class represent a message buffer allocated by nanomsg. New
+empty buffers can be allocated using C<nn_allocmsg>. Both C<nn_recv> and
+C<nn_recvmsg> can be used to allocate buffers initialised with the contents of a
+received message.
+
+C<NanoMsg::Raw::Message>s can be used to send and receive data in a zero-copy
+fashion.
+
+When using a message instance as a string, its overloading will produce a string
+containing a copy of the buffer content.
+
+A string pointing to the actual buffer content, without having to create a copy
+of it, can be obtained by dereferencing the message instance as a scalar.
+
+=method copy($data)
+
+    $msg->copy('foo');
+
+Copies C<$data> to the beginning of the buffer. If the data is larger than the
+space allocated for the buffer, an exception will be thrown.
+
+=head1 SEE ALSO
+
+L<NanoMsg::Raw>
+
 =cut
 
 1;
