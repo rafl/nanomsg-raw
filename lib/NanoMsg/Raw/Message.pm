505b6fe96d78fe6160f7968b3f7b09b3a0565c0a
Florian Ragwitz <rafl@debian.org>
2013-08-22 14:45:57 -0400

    ABSTRACT and SYNOPSIS for ::Message

--- lib/NanoMsg/Raw/Message.pm
+++ lib/NanoMsg/Raw/Message.pm
@@ -2,7 +2,25 @@ use strict;
 use warnings;
 
 package NanoMsg::Raw::Message;
+# ABSTRACT: Message buffer for NanoMsg::Raw
 
 use overload '""' => sub { ${ $_[0] } }, fallback => 1;
+
+=head1 SYNOPSIS
+
+    use NanoMsg::Raw;
+
+    {
+        my $msg = nn_allocmsg(3, 0);
+        $msg->copy('foo');
+        nn_send($sock, $msg);
+    }
+
+    {
+        nn_recv($sock, my $buf);
+        warn $buf;
+    }
+
+=cut
 
 1;
